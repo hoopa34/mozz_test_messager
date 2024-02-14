a0621d5832f62132f77a072919ff2116f2ee17ae
2918351p@gmail.com <2918351p@gmail.com>
2024-02-14 10:27:10 +0500

    Added all text

--- lib/ui/pages/page_contacts/page_contacts.dart
+++ lib/ui/pages/page_contacts/page_contacts.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mozz_test_messenger/ui/theme/app_colors/app_colors.dart';
 
+  // Страница контактов
 class PageContacts extends StatelessWidget {
   const PageContacts({super.key});
 
@@ -11,11 +13,12 @@ class PageContacts extends StatelessWidget {
   }
 }
 
-class PageContactsAppBar extends StatefulWidget implements PreferredSizeWidget {
+  // Кастомный виджет AppBar для странички контактов
+class PageContactsAppBar extends StatefulWidget implements PreferredSizeWidget { 
   @override
   final Size preferredSize;
   const PageContactsAppBar({super.key})
-      : preferredSize = const Size.fromHeight(60);
+      : preferredSize = const Size.fromHeight(169);
 
   @override
   State<PageContactsAppBar> createState() => _PageContactsAppBarState();
@@ -25,9 +28,19 @@ class _PageContactsAppBarState extends State<PageContactsAppBar> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: const Column(
+      child: Column( // Основная колонна для виджетов
+        mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          Divider(),
+          const Padding( // Чаты и поиск
+            padding: EdgeInsets.only(
+              top: 14,
+              left: 20,
+              right: 20,
+              bottom: 24,
+            ),
+            child: Column(),
+          ),
+          Divider(color: AppColors.stroke),
         ],
       ),
     );
---
+++ lib/ui/theme/app_texts/app_texts.dart
@@ -0,0 +1,9 @@
+abstract class AppTexts {
+  static const String online = 'В сети';
+  static const String offline = 'Не в сети';
+  static const String today = 'Сегодня';
+  static const String yesterday = 'Вчера';
+  static const String chats = 'Чаты';
+  static const String search = 'Поиск';
+  static const String message = 'Сообщение';
+}
