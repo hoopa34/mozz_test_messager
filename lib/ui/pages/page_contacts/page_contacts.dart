a7b39947348662a20b3b69448a22958f5a4c7097
2918351p@gmail.com <2918351p@gmail.com>
2024-02-14 11:21:16 +0500

    Rework app bar path

--- lib/ui/pages/page_contacts/page_contacts.dart
+++ lib/ui/pages/page_contacts/page_contacts.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mozz_test_messenger/ui/theme/app_colors/app_colors.dart';
-import 'package:mozz_test_messenger/ui/theme/app_texts/app_text_styles.dart';
-import 'package:mozz_test_messenger/ui/theme/app_texts/app_texts.dart';
+import 'package:mozz_test_messenger/ui/pages/page_contacts/page_contacts_app_bar.dart';
 
 // Страница контактов
 class PageContacts extends StatelessWidget {
@@ -11,74 +9,6 @@ class PageContacts extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: PageContactsAppBar(),
-    );
-  }
-}
-
-// Кастомный виджет AppBar для странички контактов
-class PageContactsAppBar extends StatefulWidget implements PreferredSizeWidget {
-  @override
-  final Size preferredSize;
-  const PageContactsAppBar({super.key})
-      : preferredSize = const Size.fromHeight(169);
-
-  @override
-  State<PageContactsAppBar> createState() => _PageContactsAppBarState();
-}
-
-class _PageContactsAppBarState extends State<PageContactsAppBar> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        // Основная колонна для виджетов
-        children: [
-          Padding(
-            // Чаты и поиск
-            padding: const EdgeInsets.only(
-              top: 14,
-              left: 20,
-              right: 20,
-              bottom: 24,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(AppTexts.chats, style: AppTestStyles.chats),
-                const SizedBox(height: 6),
-                const PageContactsAppBarSearch(),
-              ],
-            ),
-          ),
-          Divider(color: AppColors.stroke),
-        ],
-      ),
-    );
-  }
-}
-
-// Окно поиска
-class PageContactsAppBarSearch extends StatelessWidget {
-  const PageContactsAppBarSearch({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-          filled: true,
-          contentPadding: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-          fillColor: AppColors.stroke,
-          prefixIcon: const Icon(Icons.search),
-          prefixIconColor: AppColors.gray,
-          hintText: AppTexts.search,
-          border: const OutlineInputBorder(
-              borderSide: BorderSide.none,
-              borderRadius: BorderRadius.all(Radius.circular(12)))),
     );
   }
 }
---
+++ lib/ui/pages/page_contacts/page_contacts_app_bar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:mozz_test_messenger/ui/theme/app_colors/app_colors.dart';
+import 'package:mozz_test_messenger/ui/theme/app_texts/app_text_styles.dart';
+import 'package:mozz_test_messenger/ui/theme/app_texts/app_texts.dart';
+
+// Кастомный виджет AppBar для странички контактов
+class PageContactsAppBar extends StatefulWidget implements PreferredSizeWidget {
+  @override
+  final Size preferredSize;
+  const PageContactsAppBar({super.key})
+      : preferredSize = const Size.fromHeight(169);
+
+  @override
+  State<PageContactsAppBar> createState() => _PageContactsAppBarState();
+}
+
+class _PageContactsAppBarState extends State<PageContactsAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        // Основная колонна для виджетов
+        children: [
+          Padding(
+            // Чаты и поиск
+            padding: const EdgeInsets.only(
+              top: 14,
+              left: 20,
+              right: 20,
+              bottom: 24,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(AppTexts.chats, style: AppTestStyles.chats),
+                const SizedBox(height: 6),
+                const PageContactsAppBarSearch(),
+              ],
+            ),
+          ),
+          Divider(color: AppColors.stroke),
+        ],
+      ),
+    );
+  }
+}
+
+// Окно поиска
+class PageContactsAppBarSearch extends StatelessWidget {
+  const PageContactsAppBarSearch({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        filled: true,
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+        fillColor: AppColors.stroke,
+        prefixIcon: const Icon(Icons.search),
+        prefixIconColor: AppColors.gray,
+        hintText: AppTexts.search,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+        ),
+      ),
+    );
+  }
+}
