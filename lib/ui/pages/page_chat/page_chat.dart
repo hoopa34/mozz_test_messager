2b64a5d1f51a98aa7cf2e5ebabb756100a02e5a6
2918351p@gmail.com <2918351p@gmail.com>
2024-02-14 13:01:48 +0500

    Add page chat to develop

---
+++ lib/ui/pages/page_chat/page_chat.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PageChar extends StatelessWidget {
+  const PageChar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
