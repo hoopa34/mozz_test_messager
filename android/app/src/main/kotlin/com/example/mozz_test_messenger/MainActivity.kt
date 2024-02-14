7a3f861dd2d2429161047fd248b31cb0588a592b
2918351p@gmail.com <2918351p@gmail.com>
2024-02-14 09:28:08 +0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/mozz_test_messenger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mozz_test_messenger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mozz_test_messenger/ui/app.dart';
+
+void main() {
+  runApp(const App());
+}
