b5aab3c3387de9fb3f8d6d75c0ddadcaa02e7b40
2918351p@gmail.com <2918351p@gmail.com>
2024-02-15 10:24:38 +0500

    Hive integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:mozz_test_messenger/domain/hive/hive_keys.dart';
+import 'package:mozz_test_messenger/domain/hive/messenger_item_type.dart';
 import 'package:mozz_test_messenger/ui/app.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.registerAdapter(MessengerItemTypeAdapter());
+  await Hive.openBox<MessageItemType>(HiveKeys.messengerItemTypeKey);
   runApp(const App());
 }
